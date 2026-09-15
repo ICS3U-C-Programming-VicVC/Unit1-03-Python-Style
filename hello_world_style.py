3bbbe623b11371c21df357f1e6700e9fddecae3e
Victor Victor Calixte <victor.victor-calixte@stu.ocsb.ca>
2026-09-15 13:10:33 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Victor
+# Date: 09/11/26
+# Prints information.
+def main():
+    print("Information")
+    print("More Information")
+
+if __name__ == "__main__" :
+    main()
+``
